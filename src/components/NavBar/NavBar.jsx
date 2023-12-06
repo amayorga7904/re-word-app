2350f76d49aa277a07f566baaf6dd8df4a6c7e8e
amayorga7904 <alexmayorga20@gmail.com>
2023-12-05 17:09:03 -0800

    created functional navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,12 @@
 import { Link } from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 export default function NavBar({ user, setUser }) {
   function handleLogOut() {
@@ -8,13 +15,30 @@ export default function NavBar({ user, setUser }) {
   }
 
   return (
-    <nav>
-      <Link to="/api/openAi">New Prompt</Link>
-      &nbsp; | &nbsp;
-      <Link to="/api/codes">New Code</Link>
-      &nbsp; | &nbsp;
-      <span>Welcome, {user.name}</span>
-      &nbsp; | &nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
-    </nav>
-  );
+    <>
+      <Navbar key={'sm'} expand={'sm'} className="bg-body-tertiary mb-3">
+        <Container fluid>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${'sm'}`} />
+          <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${'sm'}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${'sm'}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${'sm'}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="/api/openAi">New Prompt</Nav.Link>
+                  <Nav.Link href="/api/codes">New Code</Nav.Link>
+                  <Nav.Link href="" onClick={handleLogOut}>Log Out</Nav.Link>
+                </Nav>
+          </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
+  )
 }
