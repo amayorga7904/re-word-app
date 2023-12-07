6df6c3e5f034dcc137083114e72766c0ff4c4b6f
amayorga7904 <alexmayorga20@gmail.com>
2023-12-07 10:54:20 -0800

    removed semi colons from code below

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
-import App from './pages/App/App';
-import { OpenAIProvider } from './pages/PromptHistoryPage/OpenAIAPI'; 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { CodeOpenAIProvider } from './pages/CodeHistory/CodeAIAPI';
-import "typeface-roboto";
-import { MathOpenAIProvider } from './pages/MathHistory/MathAIAPI';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
+import './index.css'
+import App from './pages/App/App'
+import { OpenAIProvider } from './pages/PromptHistoryPage/OpenAIAPI' 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { CodeOpenAIProvider } from './pages/CodeHistory/CodeAIAPI'
+import "typeface-roboto"
+import { MathOpenAIProvider } from './pages/MathHistory/MathAIAPI'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Router>
@@ -22,9 +22,9 @@ root.render(
       </MathOpenAIProvider>
     </Router>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+// reportWebVitals()
